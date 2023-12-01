be68814b26a7c9297867cd89d94aabb1f671333a
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-01 22:45:21 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int maxLiczbaProb = 3;
+            Console.WriteLine(maxLiczbaProb);
+        
+
+        }
+    }
+}

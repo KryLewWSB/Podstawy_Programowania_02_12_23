0837c5a00eeb870da562710c363c6f2ac152314f
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 19:48:17 +0100

    7

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
-﻿int liczba1 = 2;
+﻿int x = 2;
+Console.WriteLine(x);//wypisze 2
+x = 3;
+Console.WriteLine(x);//wypisze 3
 
-//3 Sposoby zwiększania zmiennych o + 1
-liczba1 = liczba1 + 1; // 2 + 1 == 3 i 3 jest przypisywana do zmiennej
-Console.WriteLine(liczba1);//3
-
-liczba1 += 1;//To samo co wtżej tylko w skrocie
-Console.WriteLine(liczba1);//4
-
-liczba1++;//To samo co wcześniej za pomocą operatora inkrementacji
-Console.WriteLine(liczba1);//5
-
-liczba1 += 10;//zwiększenie o 10 wartości w zmiennej
-Console.WriteLine(liczba1);//15
+const int i = 5;//const to stała, której nie możemy ponownie zainicjalizować inną wartością.
+//i = 5; błąd!

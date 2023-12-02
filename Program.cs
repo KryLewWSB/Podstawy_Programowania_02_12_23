f480f37bcf5ba6501de8397738bf241890c63378
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 19:24:37 +0100

    5

--- Program.cs
+++ Program.cs
@@ -1,18 +1,8 @@
-﻿byte liczba = 255;
-Console.WriteLine(liczba);
+﻿int liczba1 = 2;
+int liczba2 = 2;
 
-int liczbaTest; //Deklaracja zmiennej
-liczbaTest = 4; //Inicjalizacja (przypisanie) wartości do zmiennej.
-Console.WriteLine(liczbaTest);
-
-int liczba1 = 5,
-    liczba2 = 2;//deklaracja i incjalizacja dwóch zmiennych typu int
-
-Console.WriteLine(liczba1 + liczba2); // + jako operator arytmetyczny 5 + 2 == 7
-
-Console.WriteLine("Wynik dodawania: " + liczba1 + liczba2);// + jako operacja konkatenacji (połączenie dwóch łańcuchów znaków)
-
-Console.WriteLine("Wynik dodawania: " + 5 + 2);//+ jako Konkatenacja
-Console.WriteLine("Wynik dodawania: " + (5 + 2));//+ jako operacja arytmetyczna
-
-Console.WriteLine("Ala ma " + "kota.");//+ jako Konkatenacja
+Console.WriteLine("Dodawanie liczb: " + liczba1 + " + " + liczba2 + " = " + (liczba1 + liczba2));
+Console.WriteLine("Odejmowanie liczb: " + liczba1 + " - " + liczba2 + " = " + (liczba1 - liczba2));
+Console.WriteLine("Mnożenie liczb: " + liczba1 + " * " + liczba2 + " = " + (liczba1 * liczba2));
+Console.WriteLine("Dzielenie liczb: " + liczba1 + " / " + liczba2 + " = " + (liczba1 / liczba2));
+Console.WriteLine("Modulo liczb: " + liczba1 + " % " + liczba2 + " = " + (liczba1 % liczba2));

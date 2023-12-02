d4bc107e57cf993d9352193664859c00da78f4ad
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 18:58:16 +0100

    3

--- Program.cs
+++ Program.cs
@@ -3,5 +3,10 @@ int liczba = 5; //wartości liczby całkowite
 bool zmiennaLogiczna = true; //wartości logiczne prawda / fałsz
 double zmiennaDouble = 5.333; //wartość zmiennopozycyjna - liczby z przecinkiem
 
-Console.WriteLine(zmiennaLogiczna);
-Console.WriteLine(zmiennaDouble);
+string ciagZnakow = "Test"; //ciąg znaków / łancuch znaków
+string ciagZnakow2 = "5";
+char znak = 'T'; //Pojedynczy znak
+
+Console.WriteLine(ciagZnakow);
+Console.WriteLine(ciagZnakow2);
+Console.WriteLine(znak);

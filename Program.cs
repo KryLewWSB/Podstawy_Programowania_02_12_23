d3737bd544e2b9f44d8be36e633c78b3e8caf8e8
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 19:36:33 +0100

    6

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿int liczba1 = 2;
-int liczba2 = 2;
 
-Console.WriteLine("Dodawanie liczb: " + liczba1 + " + " + liczba2 + " = " + (liczba1 + liczba2));
-Console.WriteLine("Odejmowanie liczb: " + liczba1 + " - " + liczba2 + " = " + (liczba1 - liczba2));
-Console.WriteLine("Mnożenie liczb: " + liczba1 + " * " + liczba2 + " = " + (liczba1 * liczba2));
-Console.WriteLine("Dzielenie liczb: " + liczba1 + " / " + liczba2 + " = " + (liczba1 / liczba2));
-Console.WriteLine("Modulo liczb: " + liczba1 + " % " + liczba2 + " = " + (liczba1 % liczba2));
+//3 Sposoby zwiększania zmiennych o + 1
+liczba1 = liczba1 + 1; // 2 + 1 == 3 i 3 jest przypisywana do zmiennej
+Console.WriteLine(liczba1);//3
+
+liczba1 += 1;//To samo co wtżej tylko w skrocie
+Console.WriteLine(liczba1);//4
+
+liczba1++;//To samo co wcześniej za pomocą operatora inkrementacji
+Console.WriteLine(liczba1);//5
+
+liczba1 += 10;//zwiększenie o 10 wartości w zmiennej
+Console.WriteLine(liczba1);//15

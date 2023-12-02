2ee7af00e2aeaa1633ca92cc5460e6fdcfda3dd7
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 20:17:36 +0100

    12

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("Proszę podać liczbę 1: [zatwierdź enterem]");
-int liczba1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Proszę podać liczbę 2: [zatwierdź enterem]");
-int liczba2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine($"Działanie: {liczba1} + {liczba2} = {liczba1 + liczba2}");
+﻿
+Console.WriteLine("4 == 5 " + (4 == 5));
+Console.WriteLine("4 == 4 " + (4 == 4));
+Console.WriteLine(4 != 5);
+Console.WriteLine(4 != 4);
+Console.WriteLine(4 <= 4);
+Console.WriteLine(4 < 4);

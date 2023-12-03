b0e950a873ae09e1ff89a740aefb67181b881ff8
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 19:55:38 +0100

    23

--- Program.cs
+++ Program.cs
@@ -1,60 +1,30 @@
 ﻿
-Console.WriteLine("Proszę o podanie dnia w formie liczby(1-7): [zatwierdź enterem]");
-int day = Convert.ToInt32( Console.ReadLine() );
+Console.WriteLine("Proszę liczbę: [zatwierdź enterem]");
+int liczba = Convert.ToInt32( Console.ReadLine() );
 
-//Instrukcja switch:
-switch (day) {
-    case 1:
-        Console.WriteLine("Poniedziałek");
-        break;
-    case 2:
-        Console.WriteLine("Wtorek");
-        break;
-    case 3:
-        Console.WriteLine("Środa");
-        break;
-    case 4:
-        Console.WriteLine("Czwartek");
-        break;
-    case 5:
-        Console.WriteLine("Piątek");
-        break;
-    case 6:
-        Console.WriteLine("Sobota");
-        break;
-    case 7:
-        Console.WriteLine("Niedziela");
-        break;
-    default:
-        Console.WriteLine("Nieznany dzień tygodnia");
-        break;
-}
 
-//Instrukcja wrunkowa:
-if (day == 1)
+if (liczba > 1)
 {
-    Console.WriteLine("Poniedziałek");
-}else if (day == 2)
+    Console.WriteLine("liczba > 1");
+} 
+
+if (liczba > 2)
 {
-    Console.WriteLine("Wtorek");
-}else if (day == 3)
+    Console.WriteLine("liczba > 2");
+} 
+
+if (liczba > 3)
 {
-    Console.WriteLine("Środa");
-}else if (day == 4)
+    Console.WriteLine("liczba > 3");
+} 
+
+
+if (liczba > 4)
 {
-    Console.WriteLine("CZwartek");
-}else if (day == 5)
-{
-    Console.WriteLine("Piątek");
-}else if (day == 6)
-{
-    Console.WriteLine("Sobota");
-}else if (day == 7)
-{
-    Console.WriteLine("Niedziela");
+    Console.WriteLine("liczba > 4");
 }
 else
 {
-    Console.WriteLine("Nieznany dzień tygodnia");
+    Console.WriteLine("Inna liczba");
 }
 

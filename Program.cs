6be4202478fddaea5e129f5b01bdfd0660aa0a37
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 20:17:05 +0100

    25

--- Program.cs
+++ Program.cs
@@ -1,17 +1,44 @@
-﻿//Zadanie 1.1. Napisz program, który przyjmie od użytkownika w konsoli 2 wartości typu String i wyświetli je w konsoli użytkownikowi w ten sposób:
+﻿while (true)
+{
 //Przyjmowanie pierwszej wartości string od użytkowenika z konsoli:
-Console.WriteLine("Proszę o podanie swojego imienia: [zatwierdź enterem]");
-string imie = Console.ReadLine();
+Console.WriteLine("Proszę o podanie liczby 1: [zatwierdź enterem]");
+int liczba1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Proszę o podanie liczby 2: [zatwierdź enterem]");
+int liczba2 = Convert.ToInt16(Console.ReadLine());
 
 //Przyjmowanie drugiego wartości string od użytkowenika z konsoli:
-Console.WriteLine("Proszę o podanie swojego nazwisko: [zatwierdź enterem]");
-string nazwisko = Console.ReadLine();
+Console.WriteLine("Proszę o podanie operatora arytmetycznego (+,-,*,/): [zatwierdź enterem]");
+string operatorArytmetyczny = Console.ReadLine();//camelCase jest to sposób nazewnictwa zmiennych 
 
-//Wyświetlanie przyjętych wartości:
-Console.WriteLine("Cześć!\nTwoje imię to: " + imie + "\nA nazwisko to: " + nazwisko);//1 sposób
-Console.WriteLine($"Cześć!\nTwoje imię to: {imie}\nA nazwisko to: {nazwisko}");
 
-//3 sposób
-/*Console.WriteLine("Cześć!");
-Console.WriteLine("Twoje imię to: " + imie);
-Console.WriteLine("A nazwisko to: " + nazwisko);*/
+    switch (operatorArytmetyczny)
+    {
+        case "+":
+        {
+            Console.WriteLine($"Dodawanie: {liczba1} + {liczba2} = {liczba1 + liczba2}");
+            break;
+        }
+        case "-":
+        {
+            Console.WriteLine($"Odejmowanie: {liczba1} - {liczba2} = {liczba1 - liczba2}");
+            break;
+        }
+        case "*":
+        {
+            Console.WriteLine($"Mnożenie: {liczba1} * {liczba2} = {liczba1 * liczba2}");
+            break;
+        }
+        case "/":
+        {
+            Console.WriteLine($"Dzielenie: {liczba1} / {liczba2} = {liczba1 / liczba2}");
+            break;
+        }
+    }
+}
+
+
+/*
+ * Zadanie 3.9. Napisz program który przyjmie dwie wartości od użytkownika oraz informację o działaniu arytmetycznym, które ma wykonać program (np.. +, -, *, /).
+Należy wykorzystać instrukcję switch w celu sprawdzenia znaku operacji i wykonania operacji arytmetycznej. Proszę spróbować zapętlić program tak aby przyjmował dane od użytkownika ponownie po wykonaniu 1 operacji.
+ */

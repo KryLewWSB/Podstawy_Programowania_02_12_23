c9eae3a7283c0edcde58035e7565abf3517c5c24
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 18:31:49 +0100

    15

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 int liczba2 = 6;
 
 Console.WriteLine( liczba == liczba2);// Zwraca albo true albo false
-Console.Write("test\n");
-Console.Write("test\n");
-Console.Write("test");
+Console.WriteLine("Werfikacja: Czy " + liczba + " jest różne od " + liczba2 + " wynik: " + (liczba != liczba2));
+Console.WriteLine($"Werfikacja: Czy {liczba} jest różne od {liczba2} wynik: { liczba != liczba2 }");
+
+/*
+Console.WriteLine( "To jest liczba: " + liczba );//1 sposób wypisania zmiennej liczba
+Console.WriteLine( $"To jest liczba: {liczba}" );//2 sposób wypisania zmiennej liczba
+*/
+/*
+ * Testowy komentarz wieloliniowy
+ */

08b31da9ffbd6c9fde09305346d7460d6414137e
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 18:17:23 +0100

    14

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
-﻿int liczba = 3;
-//operator warunkowy:
-Console.WriteLine( liczba % 2 == 0 ? $"Liczba {liczba} jest parzysta!" : $"Liczba {liczba} nie jest parzysta!");
+﻿int liczba = 5;
+int liczba2 = 6;
+
+Console.WriteLine( liczba == liczba2);// Zwraca albo true albo false
+Console.Write("test\n");
+Console.Write("test\n");
+Console.Write("test");

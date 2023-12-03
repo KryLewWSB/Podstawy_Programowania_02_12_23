eb932a88495ed740acc6939417afc51161b01648
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 19:05:53 +0100

    19

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿Console.WriteLine("Proszę o podanie liczby: [zatwierdź enterem]");
-int liczba = Convert.ToInt32( Console.ReadLine() );
+﻿/*Console.WriteLine("Proszę o podanie liczby: [zatwierdź enterem]");
+int liczba = Convert.ToInt32( Console.ReadLine() );*/
 
 
-Console.WriteLine( liczba > 0 ? "Liczba jest dodatnia!" : 
-                                "Liczba jest ujemna!" );//to co w nawiasie, to operator warunkowy.
+int liczba1, liczba2, liczba3 = 0; //deklaracja 3 zmiennych typu int
+liczba1 = 5; //inicjalizacja (przypisanie) wartości do zmiennej.
+liczba2 = 25; //inicjalizacja (przypisanie) wartości do zmiennej.
+
+int liczba4 = 55;//deklaracja i inicjalizacja zmiennej typu int
+
+Console.WriteLine("Zawartość zmiennej liczba1: " + liczba1);
+Console.WriteLine("Zawartość zmiennej liczba2: " + liczba2);
+Console.WriteLine("Zawartość zmiennej liczba3: " + liczba3);
+Console.WriteLine("Zawartość zmiennej liczba4: " + liczba4);

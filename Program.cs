735b06483a852d34c793bb916df85fc54358baa3
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 19:35:00 +0100

    21

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
-﻿/*
+﻿
 Console.WriteLine("Proszę o podanie liczby: [zatwierdź enterem]");
 int liczba = Convert.ToInt32( Console.ReadLine() );
 
-if ( liczba % 2 == 0 && liczba != 0 )
+if ( liczba % 2 == 0 && liczba != 0 && liczba % 3 == 0 )
 {
-    Console.WriteLine("Liczba " + liczba + " jest podzielna przez 2!");
+    Console.WriteLine("Liczba " + liczba + " jest podzielna przez 2 i 3!");
 }
-*/
+else
+{
+    Console.WriteLine("Liczba " + liczba + " jest niepodzielna przez 2 i 3");
+}
 
-Console.WriteLine(2 == 2 || 3 < 4); // true ||  true    wypisze finalnie: True
-Console.WriteLine(2 == 2 || 3 > 4); // true ||  false    wypisze finalnie: True
-Console.WriteLine(2 != 2 || 3 < 4); // true ||  false    wypisze finalnie: True
-Console.WriteLine(2 != 2 || 3 > 4); // false ||  false    wypisze finalnie: False
 
-bool herbata = true;
-bool kawa = false;
 
-Console.WriteLine(herbata || kawa);//Idź do sklepu i kup herbatę lub kawę True
 

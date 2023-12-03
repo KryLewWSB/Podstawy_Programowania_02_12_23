f5b979dc86dec7cef7e98884792e2f3c5e9432e4
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 18:50:51 +0100

    18

--- Program.cs
+++ Program.cs
@@ -2,10 +2,5 @@
 int liczba = Convert.ToInt32( Console.ReadLine() );
 
 
-if (liczba > 0) {
-    Console.WriteLine("Liczba jest dodatnia.");
-}else if (liczba < 0) {
-    Console.WriteLine("Liczba jest ujemna.");
-}else{
-    Console.WriteLine("Liczba wynosi zero.");
-}
+Console.WriteLine( liczba > 0 ? "Liczba jest dodatnia!" : 
+                                "Liczba jest ujemna!" );//to co w nawiasie, to operator warunkowy.

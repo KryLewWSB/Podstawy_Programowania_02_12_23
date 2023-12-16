00d5a2621a7ee97d75842cefbc736c8944ede57b
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-16 18:53:20 +0100

    29_kol_4

--- Program.cs
+++ Program.cs
@@ -1,33 +1,9 @@
-﻿/* Zadanie kategorii 2 - Zweryfikuj czy liczba pobrana z konsoli jest parzysta czy nie. */
-Console.WriteLine("Podaj liczbę:");
-int liczba = Convert.ToInt16( Console.ReadLine() );
+﻿/* 4. Utwórz tablicę liczb całkowitych (minimum 5 elementów)
+ i użyj pętli foreach do wyświetlenia każdego elementu w konsoli. */
+ 
+ int[] liczby = new int[]{1, 2, 3, 4, 5};
 
-/* 1 sposób to operator warunkowy: */
-Console.WriteLine( liczba % 2 == 0 ? $"Liczba {liczba} jest podzielna przez 2!" :  
-                                     $"Liczba {liczba} NIE jest podzielna przez 2!");
-
-/* 2 Instrukcja warunkowa */
-
-if (liczba % 2 == 0) // 2 % 2 zwraca: 0 PRAWDA!          3 % 2 zwracało 1
-{
-    Console.WriteLine($"Liczba {liczba} jest podzielna przez 2!");
-}
-else
-{
-    Console.WriteLine($"Liczba {liczba} NIE jest podzielna przez 2!");
-}
-
-/* Inny sposób zapisu */
-
-if (liczba % 2 == 0) 
-    Console.WriteLine($"Liczba {liczba} jest podzielna przez 2!");
-else 
-    Console.WriteLine($"Liczba {liczba} NIE jest podzielna przez 2!");
-
-/* Inny sposób zapisu */
-
-if (liczba % 2 == 0){
-    Console.WriteLine($"Liczba {liczba} jest podzielna przez 2!");
-}else {
-    Console.WriteLine($"Liczba {liczba} NIE jest podzielna przez 2!");
-}
+ foreach (int liczba in liczby)
+ {
+     Console.WriteLine(liczba);
+ }

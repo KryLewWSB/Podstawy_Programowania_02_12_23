c10c42758cef739936cf85484c43e19450b8b725
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-16 19:50:15 +0100

    36_kol_7

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
-﻿String[] tablicaImion = new string[] { "Anna", "Tomasz", "Anita", "Adam", "Paweł", "Piotr"};
+﻿/* 7. Napisz metodę rekurencyjną w C#,
+ która przyjmuje liczbę 10 od użytkownika i oblicza sumę liczb od 1 do tej liczby.*/
 
-foreach (String imie in tablicaImion)
+ Console.WriteLine( Suma(10) );
+ 
+static int Suma(int n)
 {
-    Console.WriteLine( Powitanie(imie) ); 
-}
-
-
-static string Powitanie(string imie)
-{
-    return $"Witaj, {imie}!";
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return n + Suma(n - 1);// przekazujemy 10:  10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 wynik: 55
+    }
 }
